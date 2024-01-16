25269e411628cfa4e067d90d1cc0a71be49c4641
Andrii Hluzdovskyi <Andrii_Hluzdovskyi@epam.com>
2024-01-16 09:43:03 +0100

    Task 2

---
+++ ClassLibrary1/SimpleClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [Serializable]
+    public class SimpleClass : ISerializable
+    {
+        public int Property1 { get; set; }
+        public string Property2 { get; set; }
+
+        public SimpleClass() { }
+
+        protected SimpleClass(SerializationInfo info, StreamingContext context)
+        {
+            Property1 = info.GetInt32("Property1");
+            Property2 = info.GetString("Property2");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Property1", Property1);
+            info.AddValue("Property2", Property2);
+        }
+    }
+}
---
+++ MyOwnSerializationApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.AccessControl;
+using Common;
+
+var simpleClass = new SimpleClass { Property1 = 5, Property2 = "Test" };
+
+BinaryFormatter formatter = new BinaryFormatter();
+using (FileStream stream = new FileStream("output.dat", FileMode.OpenOrCreate))
+{
+    formatter.Serialize(stream, simpleClass);
+    Console.WriteLine("Object serialized");
+}
+
+using (FileStream stream = new FileStream("output.dat", FileMode.Open))
+{
+    SimpleClass newSimpleClass = (SimpleClass)formatter.Deserialize(stream);
+    Console.WriteLine("Object deserialized");
+    Console.WriteLine("Property 1: " + newSimpleClass.Property1);
+    Console.WriteLine("Property 2: " + newSimpleClass.Property2);
+}
